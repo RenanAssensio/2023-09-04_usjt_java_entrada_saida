e2bce166bc89ad8ee77dfdc14c89067eba9dff39
Renan Assensio <renanassensio05@gmail.com>
2023-09-04 19:38:47 -0300

    ap02(input-output): declaracao de variaveis

---
+++ SomaDoisNumeros.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+//pacote padrâo: java.lang: importado automaticamente
+public class SomaDoisNumeros{
+    public static void main(String [] args){
+     //declaração de variáveis
+     Scanner leitor = new Scanner(System.in);
+     double primeiroOperando; 
+     double segundoOperando;
+     double resultado;
+
+     //entrada
+     
+     //processamento
+
+     //saída
+    }
+}
